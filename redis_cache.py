ec8cc749bef306212503d5cce7107bcdec35f7a5
Yusuf Amer <impara1@gmail.com>
2023-12-26 14:32:01 +0100

    Implementing Singleton RedisCache ensures one instance of Redis connection is created

--- redis_cache.py
+++ redis_cache.py
@@ -6,9 +6,14 @@ REDIS_URL = os.environ["REDIS_URL"]  # Use os.environ instead of os.getenv
 
 
 class RedisCache:
-    def __init__(self):
-        # Create the Redis connection in the constructor
-        self.redis_pool = aioredis.from_url(REDIS_URL)
+    _instance = None  # Class attribute to hold the singleton instance
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(RedisCache, cls).__new__(cls)
+            # Initialize the Redis connection only once
+            cls._instance.redis_pool = aioredis.from_url(REDIS_URL)
+        return cls._instance
 
     async def get_from_redis(self, key):
         value = await self.redis_pool.get(key)
